61ec342d4caa913a0d228e33d0f1b40cedcabdaa
dwijeshMishra <60146522+DwijeshMishra@users.noreply.github.com>
2021-03-05 21:14:23 +0530

    complete spring boot with jsp repo

---
+++ fisrt/src/main/java/com/example/demo/dao/Log_Reg_dao.java
@@ -0,0 +1,14 @@
+package com.example.demo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Log_RegModel;
+
+
+
+public interface Log_Reg_dao extends JpaRepository<Log_RegModel,String> {
+
+Log_RegModel findByName(String name);
+
+
+}
