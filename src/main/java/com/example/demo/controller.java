10dc6455f843724756fd5e7cbbf93ba7cf868a87
dwijeshMishra <dwijeshvasu515@gmail.com>
2021-03-05 20:44:07 +0530

    changes

--- src/main/java/com/example/demo/controller.java
+++ src/main/java/com/example/demo/controller.java
@@ -8,12 +8,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.dao.Log_Reg_dao;
+import com.example.demo.dao.youtube_Link_Dao;
+import com.example.demo.model.Log_RegModel;
+import com.example.demo.model.youtube_Links_model;
+
 @Controller
 public class controller {
 
+	
+	String name;
 	@Autowired
 	Log_Reg_dao repo;
-	
+
+	@Autowired
+	youtube_Link_Dao yrepo;
+
 	// staring page /
 	@RequestMapping("/")
 	public String home() {
@@ -21,33 +31,74 @@ public class controller {
 	}
 
 	// loginServlet
-	@RequestMapping("login")
-	public String loginServlet(@RequestParam("name") String name, @RequestParam("pass") String pass,Model m) {
-		Log_RegModel Lr_Obj1= null;
-		Log_RegModel Lr_Obj2= null;
+	@PostMapping("login")
+	public String loginServlet(@RequestParam("name") String name,  @RequestParam("pass") String pass, Model m) {
+
+		this.name = name; // local name var from request Param goes to class level name
+		Log_RegModel Lr_Obj1 = null;
 		try {
-		Lr_Obj1=  repo.findByName(name);
-		Lr_Obj2 = repo.findByPass(pass);
-		}
-		catch (Exception e)
-		{
+			Lr_Obj1 = repo.findByName(name);
+
+		} catch (Exception e) {
 			System.out.println("User not found");
 		}
-		if (Lr_Obj1!=null)
-		{
-			m.addAttribute("name",name);
-			return "homePage.jsp";
+		if (Lr_Obj1 == null) {
+			m.addAttribute("error", "User is Not Registerd ");
+		} else {
+			if (Lr_Obj1.getName() != null && Lr_Obj1.getPass() != null) {
+				m.addAttribute("pass", pass);
+				m.addAttribute("name", name);
+				return "homePage.jsp";
+			}
 		}
-		m.addAttribute("error","User is Not Registerd !!! kindly Register First");
 		return "login.jsp";
-
 	}
 
 	@RequestMapping("register")
-	public String register(@ModelAttribute Log_RegModel lr)
-	{
+	public String register(@ModelAttribute Log_RegModel lr) {
+		
 		repo.save(lr);
-		return "login.html";
+		return "login.jsp";
 	}
 
+	@PostMapping("youtube_link")
+	public String youtubeLink(@RequestParam("subject") String subject, Model m) {
+		m.addAttribute("name", name); //global name Varibale add added after the homepage recall
+		
+		if(m.getAttribute("name")==null) {
+			return "login.jsp";
+		}
+		
+		// model object declare
+		youtube_Links_model ym;
+		
+		try {
+
+			ym = yrepo.findByYname(subject); // find subject in database
+
+			if (ym != null) // check if subject is null or not
+			{
+
+				String ylink = ym.getYlink(); // Get the You tube link with the help of ym object // return by database
+
+				m.addAttribute("ylink", ylink); // Add you tube link in model(InBuild) object
+			}
+		}
+		catch (Exception e) 
+		{
+			System.out.println("subject not found");
+		}
+		return "homePage.jsp";
+	}
+
+	@RequestMapping("book_links")
+	public String bookLink(@RequestParam("DLCD") String booklink) {
+		System.out.println(booklink);
+		return "sem1.html";
+		
+	}
+	
+	
+	
+	
 }
